547a9bb1e2fc1d5618ca650dfd766c5c0e0bb8ed
fahad-md-kamal <faahad.hossain@gmail.com>
2020-03-14 14:49:12 +0600

    Patients and followup patients created

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
-from .models import User, Shop
+from core import models
 
-admin.site.register(User)
+admin.site.register(models.User)
+admin.site.register(models.Followup)
+
+@admin.register(models.Patient)
+class ShopAdmin(OSMGeoAdmin):
+    list_display = ('first_name', 'last_name', 'location')
--- app/core/models.py
+++ app/core/models.py
@@ -14,3 +14,43 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManage()
 
     USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+class AbstractPatient(models.Model):
+    BLOOD_GROUPS = [
+        (0, 'A+'), (1, 'A-'), (2, 'B+'),(3, 'B-'),
+        (4, 'O+'),(5, 'O-'),(6, 'AB+'),(7, 'AB-'),(8, 'UNKNOWN'),
+    ]
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    nid_or_passport = models.CharField(max_length=20)   
+    profession = models.CharField(max_length=100,null=True, blank=True)
+    blood_group = models.IntegerField(choices=BLOOD_GROUPS, default=8)
+
+
+class Patient(AbstractPatient):
+    """Patient model to store patine recored"""
+    
+    
+    came_from = models.CharField(max_length=100)
+    
+    location = models.PointField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+
+    hospital_name = models.CharField(max_length=150)
+    bed_no = models.CharField(max_length=100, null=True, blank=True)
+    admitted_on = models.DateTimeField(null=True, blank=True)
+    released_on = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} - {self.last_name}'
+
+class Followup(AbstractPatient):
+    """Patinent relatives or closed ones followups"""
+    Patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} - {self.last_name}'
